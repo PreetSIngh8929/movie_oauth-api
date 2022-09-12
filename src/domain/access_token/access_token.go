86337d7e190e73a8247b0e4c8ca2882419b04500
preet <singhpreet8929@gmail.com>
2022-09-12 12:42:46 +0530

    changes in access token

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -19,11 +19,9 @@ type AccessTokenRequest struct {
 	GrantType string `json:"grant_type"`
 	Scope     string `json:"scope"`
 
-	// Used for password grant type
 	Username string `json:"username"`
 	Password string `json:"password"`
 
-	// Used for client_credentials grant type
 	ClientId     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 }
@@ -39,8 +37,6 @@ func (at *AccessTokenRequest) Validate() rest_errors.RestErr {
 	default:
 		return rest_errors.NewBadRequestError("invalid grant_type parameter")
 	}
-
-	//TODO: Validate parameters for each grant_type
 	return nil
 }
 
@@ -82,41 +78,3 @@ func (at AccessToken) IsExpired() bool {
 func (at *AccessToken) Generate() {
 	at.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
-
-// const (
-// 	expirationTime = 24
-// )
-
-// type AccessToken struct {
-// 	AccessToken string `json:"access_token"`
-// 	UserId      int64  `json:"user_id"`
-// 	ClientId    int64  `json:"client_id"`
-// 	Expires     int64  `json:"expires"`
-// }
-
-// func (at *AccessToken) Validate() *errors.RestErr {
-// 	at.AccessToken = strings.TrimSpace(at.AccessToken)
-// 	if at.AccessToken == "" {
-// 		return errors.NewBadRequestError("invalid access token id")
-// 	}
-// 	if at.UserId <= 0 {
-// 		return errors.NewBadRequestError("invalid user id")
-// 	}
-// 	if at.ClientId <= 0 {
-// 		return errors.NewBadRequestError("invalid client id")
-// 	}
-// 	if at.Expires <= 0 {
-// 		return errors.NewBadRequestError("invalid expiration time")
-// 	}
-// 	return nil
-// }
-
-// func GetNewAccessToken() AccessToken {
-// 	return AccessToken{
-// 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
-// 	}
-// }
-
-// func (at AccessToken) IsExpired() bool {
-// 	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
-// }
--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -12,16 +12,57 @@ func TestAccessTokenConstants(t *testing.T) {
 	assert.EqualValues(t, 24, expirationTime, "Expiration time shopuld be 24 hrs")
 }
 
-// func TestGetNewAccessToken(t *testing.T) {
-// 	at := GetNewAccessToken()
-// 	assert.False(t, at.IsExpired(), "brand new access token should not be expired")
-// 	assert.EqualValues(t, "", at.AccessToken, "new access token should not have defined access token id")
-// 	assert.True(t, at.UserId == 0, "new access token should not have assosciated user id")
-// }
+func TestGetNewAccessToken(t *testing.T) {
+
+	at := GetNewAccessToken(17)
+	assert.False(t, at.IsExpired(), "brand new access token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new access token should not have defined access token id")
+	assert.True(t, at.ClientId == 0, "new access token should not have assosciated client id")
+}
 
 func TestAccessTokenIsExpired(t *testing.T) {
 	at := AccessToken{}
 	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
 	assert.False(t, at.IsExpired(), "access token expiring three hours from now should not be expired")
+}
+
+func TestValidateEmptyAccessToken(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "",
+	}
+	assert.EqualValues(t, "invalid access token id", at.Validate().Message())
+}
+func TestValidateInvalidUserId(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "bcxhdYhj99",
+		UserId:      -1,
+	}
+	assert.EqualValues(t, "invalid user id", at.Validate().Message())
+}
+func TestValidateInvalidClientId(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "bcxhdYhj99",
+		UserId:      1,
+		ClientId:    -1,
+	}
+	assert.EqualValues(t, "invalid client id", at.Validate().Message())
+}
+func TestValidateInvalidExpirationTime(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "bcxhdYhj99",
+		UserId:      1,
+		ClientId:    1,
+		Expires:     -1,
+	}
+	assert.EqualValues(t, "invalid expiration time", at.Validate().Message())
+}
+func TestValidateNoError(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "bcxhdYhj99",
+		UserId:      1,
+		ClientId:    1,
+		Expires:     2,
+	}
+	assert.Nil(t, at.Validate())
 }
